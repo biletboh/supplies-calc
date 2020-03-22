e0964451fa8476f05248b350ea67a1648cb5e00c
biletboh <biletskyboh@gmail.com>
2020-03-22 20:18:58 +0200

    added calculator

---
+++ src/components/calculator.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="calculator">
+    <div class="card">
+      <div class="card-content">
+        <p>{{ calculateSupplies }}</p>
+        <div class="field">
+          <label class="label">Кількість днів</label>
+          <div class="control">
+            <input v-model="supplies.days" class="input" type="number" placeholder="Кількість днів">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Кількість людей</label>
+          <div class="control">
+            <input v-model="supplies.persons" class="input" type="number" placeholder="Кількість днів">
+          </div>
+        </div>
+        <button @click="addFoodType" class="button">Додати</button>
+      </div>
+    </div>
+    <div class="card" v-for="(foodType, index) in supplies.foodTypes" :key="index">
+      <div class="card-content">
+        <div class="supplies-form">
+          <div class="field">
+            <label class="label">Вид продуктів</label>
+            <div class="control">
+              <input v-model="foodType.name" class="input" type="text" placeholder="Вид продуктів">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Кількість прийомів їжі на день</label>
+            <div class="control">
+              <input v-model="foodType.mealCount" class="input" type="number" placeholder="Кількість прийомів їжі на день">
+            </div>
+          </div>
+          <div>--------------------------------</div>
+          <div v-for="(product, productIndex) in foodType.products" :key="productIndex">
+            <div class="supplies-form">
+              <div class="field">
+                <label class="label">Назва продукту</label>
+                <div class="control">
+                  <input v-model="product.name" class="input" type="text" placeholder="Назва продукту">
+                </div>
+              </div>
+              <div class="field">
+                <label class="label">Порцій у упаковці</label>
+                <div class="control">
+                  <input v-model="product.portions" class="input" type="number" placeholder="Порцій у упаковці">
+                </div>
+              </div>
+            </div>
+            <button @click="addProduct(index)" class="button">Додати продукт</button>
+            <div>--------------------------------</div>
+          </div>
+          <button @click="removeFoodType(index)" class="button">Видалити</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'calculator',
+  data: function () {
+    return {
+      supplies: {
+        days: 1,
+        persons: 1,
+        ration: 3,
+        foodTypes: [
+          {
+            name: '',
+            mealCount: null,
+            products: [
+              {
+                name: '',
+                baseQuantity: 1,
+                portions: ''
+              }
+            ]
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    addFoodType: function () {
+      this.supplies.foodTypes.push(
+        {
+          name: '',
+          mealCount: null,
+          products: [
+            {
+              name: '',
+              baseQuantity: 1,
+              portions: ''
+            }
+          ]
+        }
+      )
+    },
+    removeFoodType(index) {
+      this.supplies.foodTypes.splice(index, 1)
+    },
+    addProduct: function (index) {
+      this.supplies.foodTypes[index].products.push(
+        {
+          name: '',
+          baseQuantity: 1,
+          portions: ''
+        }
+      )
+    },
+    removeProduct(index, productIndex) {
+      this.supplies.foodTypes[index].products.splice(productIndex, 1)
+    },
+  },
+  computed: {
+    calculateSupplies: function () {
+      for (let foodTypes of this.supplies.foodTypes) {
+        for (let product of foodTypes.products) {
+          const productsCount = foodTypes.products.length;
+          product["proportion"] = (product.portions / this.supplies.ration / productsCount) * this.supplies.days;
+        }
+      }
+      return this.supplies
+    }
+  }
+}
+</script>
+
+<style>
+.card {
+  margin-bottom: 32px;
+}
+</style>
--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -6,29 +6,7 @@
           <div class="hello">
             <h1 class="title">{{ msg }}</h1>
             <p>Скористайтеся простим калькулятором для розрахунку запасів їжі на карантин.</p>
-            <p>{{ supplies }}</p>
-            <p>{{ foodType }}</p>
-            <div class="calculator">
-              <div class="field">
-                <label class="label">Кількість днів</label>
-                <div class="control">
-                  <input v-model="supplies.days" class="input" type="text" placeholder="Кількість днів">
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Вид продуктів</label>
-                <div class="control">
-                  <input v-model="foodType.name" class="input" type="text" placeholder="Вид продуктів">
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Кількість прийомів їжі на день</label>
-                <div class="control">
-                  <input v-model="foodType.mealCount" class="input" type="text" placeholder="Кількість прийомів їжі на день">
-                </div>
-              </div>
-              <button v-on:click="addFoodType" class="button">Додати</button>
-            </div>
+            <calculator />
           </div>
         </div>
       </div>
@@ -37,30 +15,15 @@
 </template>
 
 <script>
+import calculator from './calculator.vue'
+
 export default {
   name: 'mainPage',
   props: {
     msg: String
   },
-  data: function () {
-    return {
-      supplies: {
-        days: 0,
-        foodTypes: []
-      },
-      foodType: {
-        name: '',
-        mealCount: ''
-      }
-    }
-  },
-  methods: {
-    addFoodType: function () {
-      const type = Object.assign({}, this.foodType);
-      this.supplies.foodTypes.push(type)
-      this.foodType.name = ''
-      this.foodType.mealCount = ''
-    }
+  components: {
+    calculator
   }
 }
 </script>
